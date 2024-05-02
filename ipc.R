6165ac21892070b64acfe3c9c43ea09d9fb9dcba
juancamilogutman <gutmanjuancamilo@gmail.com>
2024-05-02 14:16:25 -0300

    Autocorrelación y correlograma

--- ipc.R
+++ ipc.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(tsibble)
 library(fable)
+library(fpp3)
 
 # ipc_ceped <- 
 #   read.xlsx("ipc_ceped_data.xlsx",
@@ -39,8 +40,36 @@ ipc <- ipc %>% select(c("periodo", "inflacion"))
 
 ipc <- ipc %>% as_tsibble(index = periodo)
                
+# Simple gráfico de la evolución en el tiempo de la inflación
 autoplot(ipc, inflacion) +
   labs(title = "Inflación mensual ",
        subtitle = "2003-2023",
        y = "Inflación")
+
+# Gráfico de dispersión entre la inflación corriente y la del periodo
+# inmediatamente anterior (esto es, si no me equivoco, un gráfico que 
+# ilustra la autocorrelación con un solo rezago:
+
+ipc %>% gg_lag(
+  inflacion, geom = "point", lags = 1) +
+  labs(x = "lag(inflacion, k)")
+
+# Lo mismo con 9 rezagos
+ipc %>% gg_lag(
+  inflacion, geom = "point", lags = 1:9) +
+  labs(x = "lag(inflacion, k)")
+
+# ACF (autocorrelation function):
+ipc %>% ACF(inflacion, lag_max = 9)
+# se observa fácil como decae la autocorrelación con cada mes
+# que nos alejamos
+
+# Correlograma:
+ipc |>
+  ACF(inflacion) |>
+  autoplot() + labs(title= "Inflacion argentina 2003-2023",
+                    subtitle= "Correlograma")
+
+# A priori no se ve estacionalidad, si se ve clara tendencia, por eso decae
+
 
